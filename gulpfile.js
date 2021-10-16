9100e9980bfd1a929325bab8f8de9250a8d1ac8c
bagasnur <hello.bagasnur@gmail.com>
2021-10-16 10:04:35 +0700

    :seedling: Initial Project

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+const {src, dest} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourceMaps = require('gulp-sourcemaps');
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer');
+const beautify = require('gulp-cssbeautify');
+const combineMQ = require('gulp-merge-media-queries');
+const comment = require('gulp-header-comment');
+const rename = require('gulp-rename');
+
+const compile = () => {
+	var sassConfig = {
+		outputStyle: 'expanded',
+		includePaths: ['./node_modules'],
+	};
+	return (
+		src('src/**/*.scss')
+			.pipe(sourceMaps.init())
+			.pipe(sass(sassConfig).on('error', sass.logError))
+			.pipe(postcss([autoprefixer()]))
+			.pipe(
+				comment(
+					`© 2021 Frastyle CSS Framework v<%= pkg.version %>. Made in Indonesia and licensed under <%= pkg.license %> License. Author: <%= pkg.author %>. Generated on <%= moment().format('MM YYYY') %>`
+				)
+			)
+			.pipe(
+				beautify({
+					autosemicolon: true,
+				})
+			)
+			.pipe(
+				combineMQ({
+					log: true,
+				})
+			)
+			.pipe(dest('dist/css'))
+	);
+};
+
+const minify = () => {
+	return src(['dist/css/**/*.css', '!dist/css/**/*.min.css'])
+		.pipe(postcss([cssnano()]))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(dest('dist/css'));
+};
+
+exports.compile = compile;
+exports.minify = minify;
---
+++ readme.md
@@ -0,0 +1,28 @@
+# ✈️ Frastyle Framework BETA
+
+**Frastyle** merupakan sebuah framework Cascading Style Sheets yang dibuat di Indonesia untuk dapat
+membantu developer dalam mengembangkan antarmuka website yang **Modern** dan **Responsif**.
+Didukung dengan dokumentasi bahasa Indonesia.
+
+_________________________________________________________________________________________________
+
+## 📌 Instalasi
+
+Opsi metode instalasi yang tersedia:
+
+* Clone the repo: `git clone git://github.com/frastyle/frastyle-framework.git`.
+
+_________________________________________________________________________________________________
+
+## 🌱 Berkontribusi
+
+_- Coming Soon_ 🔜
+
+_________________________________________________________________________________________________
+
+## 📄 Lisensi
+
+(c) 2020-2021 Dibuat dan didesain oleh [@bagasnur](https://github.com/bagasnur) di Indonesia.
+
+Konten website dibawah lisensi [Atribusi-Berbagi Serupa 2.0 Generik (CC BY-SA 2.0)](https://creativecommons.org/licenses/by-sa/2.0).
+Kode sumber dibawah [lisensi MIT](https://github.com/frastyle/frastyle-framework/blob/main/LICENSE).
