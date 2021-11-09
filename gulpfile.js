07e43ae60da5352c5b564400d9a2780843d3aa74
Bagas Nur <hello.bagasnur@gmail.com>
2021-11-09 12:49:30 +0700

    Fixed issue (#3): Must use const declaration (#4)
    Fixed issue (#3): Must use const declaration

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ const comment = require('gulp-header-comment');
 const rename = require('gulp-rename');
 
 const compile = () => {
-	var sassConfig = {
+	const sassConfig = {
 		outputStyle: 'expanded',
 		includePaths: ['./node_modules'],
 	};
